549b7cb6b87bdf07f03942bea744f4d4d15a0c5c
moniquetrevisan <moniquetrevisan@hotmail.com>
2016-03-18 22:33:02 -0300

    adicionando servlet-api e uma pagina index.jsp vazia para teste

---
+++ src/main/java/com/moniquetrevisan/urlshortening/rest/UserRs.java
@@ -0,0 +1,15 @@
+package com.moniquetrevisan.urlshortening.rest;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/users")
+public class UserRs {
+
+	@POST
+	@Produces("")
+	public void createUrl(){
+	}
+}
