9d35fc689beb7fe855f3aea67f912ac77ff0cb54
moniquetrevisan <moniquetrevisan@hotmail.com>
2016-03-19 19:30:44 -0300

    commit do primeiro rest funcionando

--- src/main/java/com/moniquetrevisan/urlshortening/rest/UserRs.java
+++ src/main/java/com/moniquetrevisan/urlshortening/rest/UserRs.java
@@ -1,15 +1,33 @@
 package com.moniquetrevisan.urlshortening.rest;
 
+import javax.annotation.ManagedBean;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 @Path("/users")
+@ManagedBean
 public class UserRs {
 
 	@POST
-	@Produces("")
-	public void createUrl(){
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{userid}/urls")
+	public void saveUrl(@PathParam ("userid") String userid,
+					 @Context HttpServletRequest request, 
+					 @Context HttpServletResponse response) {
+		String url = request.getParameter("url");
+		System.out.println(url);
+	}
+	
+	@GET
+	@Path("/itworks")
+	public String justHi() {
+		return "it works!";
 	}
 }
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -4,11 +4,9 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>URL Shortening =]</title>
+		<title>Shortening URL</title>
 	</head>
 	<body>
-
-		<h3> Hi =]</h3>
-
+		<h3>=]</h3>
 	</body>
 </html>
